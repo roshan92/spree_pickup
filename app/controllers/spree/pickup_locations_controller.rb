e33d5895aab4192e4c5c41413972280bf1304578
Roshan <icemission@gmail.com>
2019-09-14 00:22:40 +0800

    display all

--- app/controllers/spree/pickup_locations_controller.rb
+++ app/controllers/spree/pickup_locations_controller.rb
@@ -3,12 +3,12 @@ module Spree
 
     def index
       @base_scope = Spree::PickupLocation.all
-      if params[:s_id].present? && params[:c_id].present?
-        @state = Spree::State.find_by(id: params[:s_id])
-        @country = Spree::Country.find_by(id: params[:c_id])
-        @base_scope = @base_scope.includes(address: [:state, :country])
-                  .where(spree_addresses: {state_id: params[:s_id], country_id: params[:c_id]})
-      end
+#       if params[:s_id].present? && params[:c_id].present?
+#         @state = Spree::State.find_by(id: params[:s_id])
+#         @country = Spree::Country.find_by(id: params[:c_id])
+#         @base_scope = @base_scope.includes(address: [:state, :country])
+#                   .where(spree_addresses: {state_id: params[:s_id], country_id: params[:c_id]})
+#       end
       @base_scope
     end
 
